5bc4e8d30ab1fb1bd40bcc8348da82684d34f608
Patrick Becker <patrickgbecker@gmail.com>
2022-08-05 11:14:13 -0500

    Add files for PR approval

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,42 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import Hotel from './Hotel';
+import Guest from './Guest';
+import data from './data';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+
+import './images/monsieur-bekker.png';
+import './images/deluxe-full-room.png';
+import './images/deluxe-king-room.png';
+import './images/deluxe-queen-room.png';
+import './images/deluxe-twin-room.png';
+import './images/double-full-room.png';
+import './images/double-king-room.png';
+import './images/double-queen-room.png';
+import './images/double-twins-room.png';
+import './images/petite-full-room.png';
+import './images/petite-king-room.png';
+import './images/petite-queen-room.png';
+import './images/petite-twin-room.png';
+import './images/royal-full-room.png';
+import './images/royal-king-room.png';
+import './images/royal-queen-room.png';
+import './images/single-full-room.png';
+import './images/single-king-room.png';
+import './images/single-queen-room.png';
+import './images/single-twin-room.png';
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const searchButton = document.getElementById('searchBtn');
+const availableRoomsSection = document.getElementById('availableRooms');
+const bookedRooms = document.getElementById('bookedRooms');
+const searchForm = document.getElementById('searchForm');
+const userMessage = document.getElementById('userMessage');
+const loginButton = document.getElementById('loginBtn');
+
+let hotel;
+let customer;
+
+const createHotel = () => {
+  Promise.all(data.getAllHotelData())
+    .then(values => hotel = new Hotel("overLook", values[0], values[1], values[2]));
+};
