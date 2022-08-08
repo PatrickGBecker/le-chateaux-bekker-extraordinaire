2da6431ad98614dc23b6b1f51299e4ffc64809de
Patrick Becker <patrickgbecker@gmail.com>
2022-08-08 12:30:27 -0500

    Add display search Results

--- src/scripts.js
+++ src/scripts.js
@@ -100,3 +100,95 @@ const displayRewardsEarned = () => {
     Total spent ${customer.returnTotalBookingCost(hotel)}`;
   customerName.innerText = `Welcome back ${customer.name.split('')[0]}!`
 }
+
+
+const compileForData = (elements) => {
+  const data = {
+    date: elements[0].value.replace(/-/g, "/"),
+    roomType: [],
+    bedSize: [],
+    numBeds: []
+  };
+  elements.forEach(element => {
+    if (element.className.includes('room-type') && element.checked) {
+      data.roomType.push(element.value);
+    }
+    if (element.className/includes('bed-size') && element.checked) {
+      data.bedSize.push(element.value);
+    }
+    if (element.className.includes('number-of-beds') && element.checked) {
+      data.numBeds.push(element.value);
+    }
+  });
+  return data;
+}
+
+const retrieveFormValues = () => {
+  const values = document.getElementById('searchForm');
+  const data = compileFormData(Array.from(values.elements));
+  return data;
+}
+
+const displaySearchResults = (e) => {
+  e.preventDefault();
+  const data = retreiveFormValues() ;
+  const results = hotel.returnAllFilteredResults(data.date,
+    data.roomType, data.bedSize, data.numBeds);
+  const header = results.length === 0 ?
+    `We apologize, no rooms match your search details for ${fixDate(data.date)}. Please keep searching.`
+    : `Room available on ${fixDate(data.date)}`
+  if (results) {
+    const availableRooms = document.getElementById('availableRooms');
+      availableRooms.innerHTML = "";
+      availableRooms.innerHTML =
+      `<h2 class=available-rooms-header"> ${header} </h2>`;
+      results.forEach(result => {
+        availableRooms.innerHTML +=
+        `<section class="available-rooms-card">
+          <div class="hotel-img-container">
+            <img src="./images/room-${result.number}.jpg" alt="A luxuriouslt decorated, yet inviting room with a heavenly bed" class="available-rooms-card-img">
+          </div>
+          <div class="available-rooms-info-container">
+            <p class="info-left available-rooms-card-room-number">Room ${result.number}</p>
+            <p class="info-right available-rooms-card-bed-size">Bed Size - ${capitalizeWords(result.bedSize)}</p>
+            <p class="info-left available-rooms-card-room-type">${capitalizeWords(result.roomType)}</p>
+            <p class="info-right available-rooms-card-number-of-beds">Total Beds - ${result.numBeds}</p>
+            <p class="info-center available-rooms-card-has-bidet">${result.bidet ? "Complimentary Bidet!" : ""}</p>
+            <p class="info-center available-rooms-card-cost-per-night">$${result.costPerNight} / Per Night</p>
+          </div>
+          <button class="available-rooms-card-book-button book-now button" data-booking-data=${storeBookingData(data.date, result)}>Book Now</button>
+        </section>`
+      });
+      document.location.href = '#availableRooms';
+      searchForm.reset();
+      return;
+  }
+  displayUserMessage('Please enter a valid date.');
+}
+const displayUserMessage = (content) => {
+  const message = document.getElementById('messsage');
+  message.innerText = content;
+  toggleHidden(userMessage, 'false');
+}
+
+let fixDate = (date) => {
+  const splitDate = date.split('/');
+  splitDate.push(splitDate.shift());
+  const joined = splitDate.join('/');
+  return joined;
+}
+
+const capitalizeWords = (string) => {
+  let words = string.split(' ');
+  let capitalized = words.map( word => word.charAt(0).toUpperCase() + word.slice(1, word.length))
+  return capitalized.join(' ');
+}
+
+const storeBookingData = (date, data) => {
+  const bookingData = {
+    userID: customer.id,
+    date: date,
+    roomNumber: data.number
+  };
+  return JSON.stringify(bookingData);
+}
