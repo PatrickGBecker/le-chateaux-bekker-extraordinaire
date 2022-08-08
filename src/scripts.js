397818b3be8aa422dc67e775710b7a8e81be52db
Patrick Becker <patrickgbecker@gmail.com>
2022-08-08 10:45:27 -0500

    Refactor scripts and classes

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import Hotel from './Hotel';
-import Guest from './Guest';
+import Customer from './Customer';
 import data from './data';
 
 
@@ -40,3 +40,63 @@ const createHotel = () => {
   Promise.all(data.getAllHotelData())
     .then(values => hotel = new Hotel("overLook", values[0], values[1], values[2]));
 };
+
+const toggleHidden = (element, hidden = 'true') => element.setAttribute('aria-hidden', hidden);
+
+const creatUser = (e) => {
+  e.preventDefault();
+  const userName = document.getElementById('userNameInput').value;
+  Promise.resolve(data.getUserData(parseInt(findUserID(userName)), () => showLoginError()))
+    .then(value => {
+      customer = new Customer(value);
+      login();
+      displayRooms();
+      displayRewardsEarned();
+    })
+};
+
+const login = () => {
+  const password = document.getElementById('passwordInput').value;
+  if (password !== 'bekker2021') {
+    showLoginError();
+    return;
+  }
+  showMain();
+}
+
+const disableButton = (date) => {
+  if (new Date(date) < new Date()) {
+    return 'disabled';
+  }
+  return;
+}
+
+const displayRooms = () => {
+  const bookedRooms = document.getElementById('bookedRooms');
+  bookedRooms.innerHTML = '';
+  customer.organizeBookingsByDate(hotel).forEach(room => {
+    const section = document.createElement('section');
+    section.className = 'booked-room-card';
+    section.tabIndex = "0";
+    section.dataset.bookingID = room.bookingID;
+    secetion.innerHTML =
+    `<img src="./images/room-${room.number}.jpg" alt="Hotel room with a bed and desk" class="booked-room-card-img">
+      <p class="booked-room-card-room-number">Room ${room.number}</p>
+      <p class="booked-room-card-date-booked">${fixDate(room.dateBooked)}</p>
+      <p class="booked-room-card-type"> ${capitalizeWords(room.roomType)}</p>
+      <p class="booked-room-card-bed-size">Bed Size ${capitalizeWords(room.bedSize)}</p>
+      <p class="booked-room-card-number-of-beds">Beds: ${room.numBeds}</p>
+      <p class="booked-room-card-cost">${room.costPerNight} / Night</p>
+      <button class="booked-room-ard-button button" ${disableButton(room.dateBooked)}>Cancel Reservation</button>`;
+    bookedRooms.append(section);
+  });
+};
+
+const displayRewardsEarned = () => {
+  const rewardsEarned = document.getElementById('rewardsEarned');
+  const customerEarned = document.getElementById('cusrtomerName')
+  rewardsEarned.innerText =
+    `You have garnered ${customer.returnRewardsEarned(hotel)} rewards
+    Total spent ${customer.returnTotalBookingCost(hotel)}`;
+  customerName.innerText = `Welcome back ${customer.name.split('')[0]}!`
+}
